24ff07339d721e2a22053749a8e0736c0fb729ef
Juan Fernando <juanfer@pc-83.udea.edu.co>
2017-08-09 00:24:05 -0500

    Ya se mueve la bolita y colisiona con los muros.

--- public/index.js
+++ public/index.js
@@ -13,45 +13,82 @@ class Bar{
 }
 
 class Ball {
-  constructor( r = 10) {
-    //this.x = x;
-    //this.y = y;
+  constructor(r) {
     this.r = r;
     this.pos = new VecPos;
+    this.speed = new VecPos;
   }
 }
 
+class Player {
+  constructor() {
+    this.score = 0;
+
+  }
+}
 var counter = 0;
 var time = 20;
 var canvas = document.getElementById('pong');
 var contex = canvas.getContext('2d');
-
+const rad = 10;
 
 function setBackground() {
-  //Board
+  contex.beginPath();
   contex.fillStyle = 'pink';
   contex.fillRect(0, 0, canvas.width, canvas.height);
-  //Ball
-  contex.fillStyle = 'yellow';
+}
+function setBall() {
   contex.arc(ball.pos.x, ball.pos.y, ball.r, 0, 2*Math.PI);
+  contex.fillStyle = 'yellow';
   contex.fill();
   contex.stroke();
-
-  //Bars
-  contex.fillStyle = 'white';
-  contex.fillRect(10, 10, 10, 50);
-
 }
-
-const ball = new Ball;
+function setLeftBar() {
+  contex.fillStyle = 'white';
+  contex.fillRect(leftBar.pos.x, leftBar.pos.y, leftBar.size.x, leftBar.size.y);
+}
+function setRightBar() {
+  contex.fillStyle = 'white';
+  contex.fillRect(rightBar.pos.x, rightBar.pos.y, rightBar.size.x, rightBar.size.y);
+}
+const ball = new Ball(rad);
 ball.pos.x = 100;
 ball.pos.y = 100;
+ball.speed.x = 100;
+ball.speed.y = 100;
 
+const leftBar = new Bar(10, 50);
+leftBar.pos.x = 10;
+leftBar.pos.y = 10;
+const rightBar = new Bar(10, 50);
+rightBar.pos.x = canvas.width - 20;
+rightBar.pos.y = canvas.height - 60;
+
+//Calculate time to draw with request animation frame
+let lasTime;
+function callback(millisec) {
+  if (lasTime) {
+    update((millisec - lasTime)/1000); //convert to seconds
+  }
+  lasTime = millisec;
+  requestAnimationFrame(callback);
+}
 function update(dt) {
-  ball.pos.x =+ xspeed * dt;
-  ball.pos.y =+ yspeed * dt;
-}
-function init() {
+  ball.pos.x += ball.speed.x * dt;
+  ball.pos.y += ball.speed.y * dt;
+
+  if((ball.pos.x + rad) > canvas.width ||
+          (ball.pos.x - rad) < 0){
+    ball.speed.x = - ball.speed.x;
+  }
+
+  if((ball.pos.y + rad) > canvas.height ||
+          (ball.pos.y - rad) < 0){
+    ball.speed.y = - ball.speed.y;
+  }
   setBackground();
+  setBall();
+  setLeftBar();
+  setRightBar();
 }
-init();
+callback();
